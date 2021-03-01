43cdd93ce0e0780021877911933e0263f75641db
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-03-01 03:25:11 +0000

    Commit 1 Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,39 @@ class UsersController < ApplicationController
     render({ :template => "users/index.html" })
   end
 
+  def new_registration_form
+    
+    render({ :template => "users/signup_form.html.erb" })
+  end
+
+  def new_session_form
+    
+    render({ :template => "users/signin_form.html.erb" })
+  end
+
+  def toast_cookies
+    reset_session
+    
+    redirect_to("/", {:notice => "See ya later!"})
+  end
+
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un}).at(0)
+    if user == nil
+      redirect_to("/user_sign_in", {:alert => "No one by that name 'round these parts"})
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", {:notice => "Welcome back, "+ user.username + "!"})
+      else
+        redirect_to("/user_sign_in", {:alert => "Nice try, sucker!"})
+      end
+    end
+  end
+
   def show
     the_username = params.fetch("the_username")
     @user = User.where({ :username => the_username }).at(0)
@@ -16,10 +49,18 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", {:notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", {:alert => user.errors.full_messages.to_sentence})
+    end
   end
 
   def update
